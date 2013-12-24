c642d8455e478dc26fa3ecf0ab7de2f89e986d4c
Andrew B. Smith <drewsmits@gmail.com>
2013-12-23 19:10:53 -0800

    Added back the sample app

---
+++ ConductorTestApp/ConductorTestApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ConductorTestApp
+//
+//  Created by Andrew Smith on 12/23/13.
+//  Copyright (c) 2013 Andrew Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
