45f729d303271c5bbb6f24cc2037ef695e076616
Andrew B. Smith <drewsmits@gmail.com>
2013-12-23 19:15:56 -0800

    Added missing files

---
+++ ConductorTestApp/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  ConductorApp
+//
+//  Created by Andrew Smith on 12/12/12.
+//  Copyright (c) 2012 Andrew B. Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Conductor/Conductor.h"
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (nonatomic, strong) CDQueueController *mainQueueController;
+
+@end
---
+++ ConductorTestApp/CDMainViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  ConductorApp
+//
+//  Created by Andrew Smith on 12/12/12.
+//  Copyright (c) 2012 Andrew B. Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CDMainViewController : UITableViewController
+
+- (IBAction)runLongTask:(id)sender;
+- (IBAction)runIsExecutingTasks:(id)sender;
+- (IBAction)runAndCancel:(id)sender;
+
+@end
